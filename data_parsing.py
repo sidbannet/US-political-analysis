b31f8f39c302bab7f18e1d60ef8c38a505b08763
Siddhartha Banerjee <sidban@uwalumni.com>
2020-06-21 09:45:50 -0700

    Read data from database
    Initial code commit to read data from database.

--- data_parsing.py
+++ data_parsing.py
@@ -1,30 +1,22 @@
-## Import system tools
+## Load packages
 import os
-import subprocess
-import sys
-
-## Setup path correctly
-repo_dir = subprocess.check_output(
-    'git rev-parse --show-toplevel', shell=True,
-).decode('utf-8').strip()
-sys.path.insert(0, repo_dir + os.sep + 'stat-tools')
-##
 import pandas as pd
 
-##
+## Initialize
 df_covid_dead = pd.DataFrame()
 df_election_pres = pd.DataFrame()
-##
+## Folder path defined
 us_data_folder = r'COVID-19' \
     + os.sep + 'csse_covid_19_data' \
     + os.sep + 'csse_covid_19_time_series' \
     + os.sep
 us_election_folder = r'US-election-data' + os.sep
-##
+## COVID US death record added
 df_covid_dead = pd.read_csv(
     us_data_folder + 'time_series_covid19_deaths_US.csv',
 )
-##
+## Election data record added
 df_election_pres = pd.read_csv(
     us_election_folder + 'countypres_2000-2016.csv',
 )
+
